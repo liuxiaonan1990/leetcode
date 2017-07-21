119748a059fdcc1dbd280d4be00119646f3403b6
liuxiaonan <liuxiaonan1990@126.com>
2017-07-21 16:40:53 +0800

    finish 32

--- 32. Longest Valid Parentheses_new.py
+++ 32. Longest Valid Parentheses_new.py
@@ -6,7 +6,7 @@ class Solution(object):
 
     def fillSpace(self, l, r, s):
 
-        while l > 0 and r < len(s):
+        while l > 0 and r < len(s) - 1:
             if self.s_dict[s[l - 1]] == -1 and self.s_dict[s[r + 1]] == 1:
                 self.space[l - 1] = 1
                 self.space[r + 1] = 1
@@ -17,6 +17,126 @@ class Solution(object):
                 return r
 
         return r
+
+
+    def fillSpace_new2(self, l, r, s):
+        while True:
+    #        print l, r
+    #        print self.space
+            if l == 0 or r == len(s) - 1:
+                return [l, r]
+
+            if self.space[l - 1] == 1:
+                i = l - 1
+                while i >= 0:
+                    if self.space[i] == 1:
+                        i -= 1
+                        continue
+                    else:
+                        i += 1
+                        break
+
+                l = i
+
+            if self.space[r + 1] == 1:
+                j = r + 1
+                while j < len(s):
+                    if self.space[j] == 1:
+                        j += 1
+                        continue
+                    else:
+                        j -= 1
+                        break
+
+                r = j
+
+            if l <= 0 or r >= len(s) - 1:
+                return [l, r]
+
+            if self.s_dict[s[l - 1]] == -1 and self.s_dict[s[r + 1]] == 1:
+                self.space[l - 1] = 1
+                self.space[r + 1] = 1
+
+                l -= 1
+                r += 1
+
+            else:
+                return [l, r]
+
+
+
+    def fillSpace_new(self, l, r, s):
+        while True:
+            print l , r
+            if l == 0 and r == len(s) - 1:
+                return [l, r]
+
+            if l == 0 and r < len(s) - 1:
+                if self.space[r + 1] == 1:
+                    j = r + 1
+                    while j < len(s):
+                        if self.space[j] == 1:
+                            j += 1
+                            continue
+                        else:
+                            break
+
+                    r = j - 1
+
+                return [l, r]
+
+            if r == len(s) - 1 and l > 0:
+                if self.space[l - 1] == 1:
+                    i = l - 1
+                    while i > 0:
+                        if self.space[i] == 1:
+                            i -= 1
+                            continue
+                        else:
+                            break
+
+                    l = i + 1
+
+                return [l, r]
+
+
+
+            if self.space[l - 1] == 1:
+                i = l - 1
+                while i >0:
+                    if self.space[i] == 1:
+                        i -= 1
+                        continue
+                    else:
+                        break
+
+                l = i
+
+            if self.space[r + 1] == 1:
+                j = r + 1
+                while j < len(s):
+                    if self.space[j] == 1:
+                        j += 1
+                        continue
+                    else:
+                        break
+
+                r = j
+
+
+            if l <= 0 or r >= len(s) - 1:
+                return [l, r]
+
+            if self.s_dict[s[l - 1]] == -1 and self.s_dict[s[r + 1]] == 1:
+                self.space[l - 1] = 1
+                self.space[r + 1] = 1
+
+                l -= 1
+                r += 1
+
+            else:
+                return [l, r]
+
 
     def findLongest(self, space):
         l = 0
@@ -75,7 +195,7 @@ class Solution(object):
                 self.space[i] = 1
                 self.space[i + 1] = 1
                 ret = self.fillSpace(i, i + 1, s)
-                print self.space
+            #    print self.space
 
                 i = ret + 1
             else:
@@ -84,16 +204,19 @@ class Solution(object):
         i = 0
         while i + 1 < len(self.space):
             if self.space[i] == 0 and self.space[i + 1] == 1:
-                for j in range(i + 2, len(self.space)):
+                for j in range(i + 2, len(self.space) - 1):
                     if self.space[j] == 1 and self.space[j + 1] == 0:
-                        ret = self.fillSpace(i + 1, j, s)
-                        print self.space
+                        ret = self.fillSpace_new2(i + 1, j, s)
+                 #       print self.space
 
-                        i = ret + 1
+                        i = ret[1]
                         break
+
+                i += 1
 
             else:
                 i += 1
+
 
 
         return self.findLongest(self.space)
